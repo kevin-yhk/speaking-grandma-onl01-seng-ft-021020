e0c0b10808008abe6c1a6afd1852009845535465
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-11 04:49:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string != string.upcase
+  if string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
