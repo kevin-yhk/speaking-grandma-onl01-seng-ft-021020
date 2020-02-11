692598aafbea225c811e9d6418d5767cce766917
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-11 04:58:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,5 +19,9 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+    return string "NO, NOT SINCE 1938!"
+  else
+    return string 
   end
 end
